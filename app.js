3b6a77323b4b3a3ecf60d6b2a8fbf86b24038c25
Matthew G <matt.gaba@gmail.com>
2018-01-16 10:30:45 -0500

    Implemented log Middleware

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.listen(3000, () => console.log('Server listening'));
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
     res.send('Welcome');
-})
+    next();
+});
+
+app.get('/news', function(req, res, next) {
+    res.send('This is the news');
+    next();
+});
+
+app.use((req, res, next) => {
+   const method = req.method;
+   const path = req.path;
+   console.log(`${method} ${path} ${res.statusCode}`);
+});
